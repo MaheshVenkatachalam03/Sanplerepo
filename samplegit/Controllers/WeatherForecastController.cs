08a3a46c5cb4d2cae3998d00d737505a3acfc859
Mahesh <mahi97950@gmail.com>
2022-04-26 11:25:42 +0530

    updated

--- samplegit/Controllers/WeatherForecastController.cs
+++ samplegit/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace samplegit.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
